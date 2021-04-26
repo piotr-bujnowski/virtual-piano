0ed412adc87a615ea0611bf449a3f27a3f8d4e1f
Piotr Bujnowski <bujan98pl@gmail.com>
2021-04-26 19:44:24 +0200

    Implement sound on cursor click

--- src/js/main.js
+++ src/js/main.js
@@ -1,56 +1,122 @@
 document.addEventListener("keypress", function (e) {
     if (e.code === "KeyA") {
-        let aKeySound = new Audio("media/A.mp3");
+        playKey("A")
         console.log("The 'A' key is pressed.");
-        aKeySound.play();
     }else if (e.code === "KeyS"){
-        let sKeySound = new Audio("media/S.mp3");
+        playKey("S")
         console.log("The 'S' key is pressed.");
-        sKeySound.play();
     }else if (e.code === "KeyD"){
-        let dKeySound = new Audio("media/D.mp3");
+        playKey("D")
         console.log("The 'D' key is pressed.");
-        dKeySound.play();
     }else if (e.code === "KeyF"){
-        let fKeySound = new Audio("media/F.mp3");
+        playKey("F")
         console.log("The 'F' key is pressed.");
-        fKeySound.play();
     }else if (e.code === "KeyG"){
-        let gKeySound = new Audio("media/G.mp3");
+        playKey("G")
         console.log("The 'G' key is pressed.");
-        gKeySound.play();
     }else if (e.code === "KeyH"){
-        let hKeySound = new Audio("media/H.mp3");
+        playKey("H")
         console.log("The 'H' key is pressed.");
-        hKeySound.play();
     }else if (e.code === "KeyJ"){
-        let jKeySound = new Audio("media/J.mp3");
+        playKey("J")
         console.log("The 'J' key is pressed.");
-        jKeySound.play();
     }else if (e.code === "KeyW"){
-        let jKeySound = new Audio("media/W.mp3");
+        playKey("W")
         console.log("The 'W' key is pressed.");
-        jKeySound.play();
     }else if (e.code === "KeyE"){
-        let jKeySound = new Audio("media/E.mp3");
+        playKey("E")
         console.log("The 'E' key is pressed.");
-        jKeySound.play();
     }else if (e.code === "KeyT"){
-        let jKeySound = new Audio("media/T.mp3");
+        playKey("T")
         console.log("The 'T' key is pressed.");
-        jKeySound.play();
     }else if (e.code === "KeyY"){
-        let jKeySound = new Audio("media/Y.mp3");
+        playKey("Y")
         console.log("The 'Y' key is pressed.");
-        jKeySound.play();
     }else if (e.code === "KeyU"){
-        let jKeySound = new Audio("media/U.mp3");
+        playKey("U")
         console.log("The 'U' key is pressed.");
-        jKeySound.play();
     }else {
         console.log("Error: " + e.code + " is an unbound key!")
     }
 });
+
+let aKey = document.querySelector(".white-keys:nth-child(1)");
+let sKey = document.querySelector(".white-keys:nth-child(2)");
+let dKey = document.querySelector(".white-keys:nth-child(3)");
+let fKey = document.querySelector(".white-keys:nth-child(4)");
+let gKey = document.querySelector(".white-keys:nth-child(5)");
+let hKey = document.querySelector(".white-keys:nth-child(6)");
+let jKey = document.querySelector(".white-keys:nth-child(7)");
+
+let wKey = document.querySelector(".w");
+let eKey = document.querySelector(".e");
+let tKey = document.querySelector(".t");
+let yKey = document.querySelector(".y");
+let uKey = document.querySelector(".u");
+
+aKey.addEventListener("click", function (e) {
+   playKey("A");
+});
+
+sKey.addEventListener("click", function (e) {
+    playKey("S");
+});
+
+dKey.addEventListener("click", function (e) {
+    playKey("D");
+});
+
+fKey.addEventListener("click", function (e) {
+    playKey("F");
+});
+
+gKey.addEventListener("click", function (e) {
+    playKey("G");
+});
+
+hKey.addEventListener("click", function (e) {
+    playKey("H");
+});
+
+jKey.addEventListener("click", function (e) {
+    playKey("J");
+});
+
+wKey.addEventListener("click", function (e) {
+    playKey("W");
+});
+
+eKey.addEventListener("click", function (e) {
+    playKey("E");
+});
+
+tKey.addEventListener("click", function (e) {
+    playKey("T");
+});
+
+yKey.addEventListener("click", function (e) {
+    playKey("Y");
+});
+
+uKey.addEventListener("click", function (e) {
+    playKey("U");
+});
+
+let header = document.querySelector("h1");
+header.addEventListener("click", function (e) {
+   playKey("bMaj7");
+});
+
+let header2 = document.querySelector("h2");
+header2.addEventListener("click", function (e) {
+    playKey("bMaj7");
+});
+
+
+function playKey(key) {
+    let keySound = new Audio("media/" + key + ".mp3");
+    keySound.play();
+}
 
 
 
